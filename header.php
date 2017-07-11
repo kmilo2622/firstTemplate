2c7b058c8825a56e15df325d2253722ea428dbf4
Juan Camilo Arroyave Rico <desarrolloweb@dynamicweb.co>
2017-07-11 15:48:59 -0500

    Avance en la confección del template

--- header.php
+++ header.php
@@ -5,8 +5,40 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+    <header>
+        <!-- Barra de Navegación -->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Nombre Empresa</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Inicio</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Inicio<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Page 1-1</a></li>
+                                <li><a href="#">Page 1-2</a></li>
+                                <li><a href="#">Page 1-3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Final de la barra de navegación -->
+    </header>
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php include 'header.php'; ?>
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid text-center">
+    <div class="container-jumbotron">
+        <div class="row">
+            <h1>Nombre Empresa</h1>
+            <p>Descripción de la empresa.</p>
+        </div>
+    </div>
+</div>
+
+
+<!-- Final de Jumbotron -->
+<?php include 'footer.php'; ?>
