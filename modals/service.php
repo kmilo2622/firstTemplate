4c68c1cd6ead44aef4440c7a87bd57d6fe1229fd
Juan Camilo Arroyave Rico <desarrolloweb@dynamicweb.co>
2017-07-29 11:05:32 -0500

    Cambio de imagenes

---
+++ modals/service.php
@@ -0,0 +1,246 @@
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio8" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Radicación ante Entidades Públicas</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="img/gobierno.jpg" alt="Radicaciones" style="width:100%">
+                <br><br>
+                <p>
+                    <strong>Radicación ante Entidades Públicas</strong> <br>
+                    Radicamos o entregamos cualquier tipo de documento. <br> <br>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio7" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Radicación ante Entidades Privadas</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="img/radicacion-empresa.jpg" alt="Radicaciones" style="width:100%">
+                <br><br>
+                <p>
+                    <strong>Radicación ante Entidades Privadas</strong> <br>
+                    Radicamos o entregamos cualquier tipo de documento.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio6" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Diligencias Empresariales</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="img/diligencia-empresa.jpg" alt="Radicaciones" style="width:100%">
+                <br><br>
+                <p>
+
+                    <strong>Diligencias Empresariales</strong> <br>
+                    Este servicio fue diseñado para  todas las empresas que no tienen mensajero, o que el mensajero no alcanza con todas las entregas o radicaciones  del dia. Sin tener que pagar salarios y prestaciones, con nosotros solo paga por diligencia. <br><br>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio5" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Liquidación y Pago de Impuestos para Carros y Motos</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="https://c1.staticflickr.com/3/2166/2247897607_3d1271e420_b.jpg" alt="Radicaciones" style="width:100%">
+                <br><br>
+                <p>
+                    Ya no tienes que preocuparte, por buzcar quien liquide tus impuestos, y despues ir a hacer largas filas para pagar tu impuesto. <br>
+                    <strong>Nosotros lo hacemos por ti.</strong> <br>
+                    (*) Este servicio solo es para bogota, cundinamarca, meta, y antioquia.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio4" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Mensajería para E-Commerce o Ventas por Internet</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="http://www.moaconsulting.es/wp-content/uploads/2014/01/E-commerce.png" alt="Radicaciones" style="width:100%">
+                <br><br>
+                <p>
+                    Este servicio esta diseñado para establecer la alianza perfecta, entre las tiendas virtuales y <strong>Tu Aliado Express</strong> para que tu unica preocupacion sea vender y nosotros nos encargamos de entregar  tus productos. <br>
+                    Recuerda tu vendes, nosostros entregamos por ti.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio3" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Radicación de Facturas</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="http://www.riesgoymorosidad.com/wp-content/uploads/2008/07/facturas.jpg" alt="Radicaciones" style="width:100%">
+                <br><br>
+                <p>
+                    Recogemos y entregamos sus facturas en la fecha y hora que nos indiquen, somos expertos, nuestra experiencia garantiza el servicio. <br>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio2" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Diligencias Personales</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="img/fila.jpg" alt="Diligencias" style="width:100%">
+                <br><br>
+                <p>
+                    <strong>Diligencias Personales</strong> <br>
+                    Para usted señor profecional independiente, estudiante universitario, o  personas naturales les tenemos los siguientes servicios: <br>
+                    <ul>
+                        <li>Radicacion de cuentas de cobro</li>
+                        <li>Consignaciones</li>
+                        <li>Pagos de Créditos</li>
+                        <li>Pagos de Créditos o Tarjeta de Crédito</li>
+                        <li>Pagos de universidad</li>
+                        <li>Diligencias ante universidad</li>
+                        <li>Entre otras...</li>
+                    </ul>
+                    <br>
+                    Le recojemos todos lo objetos olvidados y se los entregamos donde nos indique: <br> <br>
+                    <ul>
+                        <li>Documentos</li>
+                        <li>Gafas</li>
+                        <li>Carné Empresarial</li>
+                        <li>Carné Estudiantil</li>
+                        <li>Celular</li>
+                        <li>Cargador</li>
+                        <li>Trabajos</li>
+                        <li>Entre otros...</li>
+                    </ul>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final de modal de servicio -->
+
+<!-- Inicio del Modal de Servicio -->
+<div id="servicio1" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Trámites</h4>
+            </div>
+            <div class="modal-body">
+                <img class="img-noticia-detalle" src="https://computernewagedotcom.files.wordpress.com/2015/06/mensajeria-instantanea1.png" alt="Trámites" style="width:100%">
+                <br><br>
+                <p>
+                    Son todas las radicaciones, o incripciones, para las cuales se requiere de mucho tiempo, ya que se debe tomar turno, o hacer largas filas.
+                </p>
+                <p>
+                    <strong>Ejemplo:</strong> <br>
+                    <ul>
+                        <li>Afiliación a EPS</li>
+                        <li>Afiliación a ARL</li>
+                        <li>Afiliación a CCF</li>
+                        <li>Inscripción al RIT</li>
+                        <li>Entre otros...</li>
+                    </ul>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- Final del Modal de Servicio -->
--- spaces/servicios.php
+++ spaces/servicios.php
@@ -7,178 +7,139 @@
             <!-- Inicio del Servicio -->
             <div class="col-md-3">
                 <div class="thumbnail">
-                    <a href="">
+                    <a href="" data-toggle="modal" data-target="#servicio1">
                         <div class="caption text-center">
-                            <h3>Mensajería <br> <br></h3>
+                            <h3>Trámites <br> <br></h3>
                             <br>
                         </div>
-                        <img src="https://computernewagedotcom.files.wordpress.com/2015/06/mensajeria-instantanea1.png" class="img-circle img-service" alt="Cinque Terre" width="200" height="200">
+                        <img src="https://computernewagedotcom.files.wordpress.com/2015/06/mensajeria-instantanea1.png" class="img-circle img-service" alt="Trámites" width="200" height="200">
                     </a>
                     <div class="caption">
                         <br>
-                        <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
                     </div>
                 </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Trámites y Diligencias<br></h3>
-                                <br>
-                            </div>
-                            <img src="http://elsalvador.eregulations.org/media/hojita_93.png" class="img-circle img-service" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <br>
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Radicación de Facturas</h3>
-                                <br>
-                            </div>
-                            <img src="http://www.riesgoymorosidad.com/wp-content/uploads/2008/07/facturas.jpg" class="img-circle img-service" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <br>
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Trámites Empresariales</h3>
-                                <br>
-                            </div>
-                            <img src="http://guatemala.eregulations.org/media/empresa_1.jpg" class="img-circle img-service" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <br>
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Trámites Personales <br> <br></h3>
-                                <br>
-                            </div>
-                            <img src="http://www.segelsoft.com/wp-content/uploads/2011/05/Empresa.jpg" class="img-circle img-service" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <br>
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Radicación ante Entidades Públicas <br> <br></h3>
-                            </div>
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Villahermosa.Palacio_de_Gobierno_01.JPG/1200px-Villahermosa.Palacio_de_Gobierno_01.JPG" class="img-circle img-service radicaciones" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Radicación ante Entidades Privadas <br> <br></h3>
-                            </div>
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Palacio_De_La_Gobernaci%C3%B3n.JPG/1200px-Palacio_De_La_Gobernaci%C3%B3n.JPG" class="img-circle img-service radicaciones" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                <br>
-                                <a href="">Leer Más...</a>
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
-
-                <!-- Inicio del Servicio -->
-                <div class="col-md-3">
-                    <div class="thumbnail">
-                        <a href="">
-                            <div class="caption text-center">
-                                <h3>Mensajería para E-Commerce <br> <br></h3>
-                            </div>
-                            <img src="http://www.moaconsulting.es/wp-content/uploads/2014/01/E-commerce.png" class="img-circle img-service radicaciones" alt="Cinque Terre" width="200" height="200">
-                        </a>
-                        <div class="caption">
-                            <!-- Opcional -->
-                            <!-- <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            </p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- Final del Servicio -->
             </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href=""  data-toggle="modal" data-target="#servicio2">
+                        <div class="caption text-center">
+                            <h3>Diligencias Personales <br></h3>
+                            <br>
+                        </div>
+                        <img src="img/fila.jpg" class="img-circle img-service" alt="Diligencias" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href=""  data-toggle="modal" data-target="#servicio6">
+                        <div class="caption text-center">
+                            <h3>Diligencias Empresariales <br></h3>
+                            <br>
+                        </div>
+                        <img src="img/diligencia-empresa.jpg" class="img-circle img-service" alt="Diligencias" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href="" data-toggle="modal" data-target="#servicio3">
+                        <div class="caption text-center">
+                            <h3>Radicación de Facturas <br></h3>
+                            <br>
+                        </div>
+                        <img src="http://www.riesgoymorosidad.com/wp-content/uploads/2008/07/facturas.jpg" class="img-circle img-service" alt="Radicaciones" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href="" data-toggle="modal" data-target="#servicio7">
+                        <div class="caption text-center">
+                            <h3>Radicación ante Entidades Privadas</h3>
+                            <br>
+                        </div>
+                        <img src="img/radicacion-empresa.jpg" class="img-circle img-service" alt="Radicaciones" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href="" data-toggle="modal" data-target="#servicio8">
+                        <div class="caption text-center">
+                            <h3>Radicación ante Entidades Públicas</h3>
+                            <br>
+                        </div>
+                        <img src="img/gobierno.jpg" class="img-circle img-service" alt="Radicaciones" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href="" data-toggle="modal" data-target="#servicio4">
+                        <div class="caption text-center">
+                            <h3>Mensajería para E-Commerce <br><br></h3>
+                        </div>
+                        <img src="http://www.moaconsulting.es/wp-content/uploads/2014/01/E-commerce.png" class="img-circle img-service radicaciones" alt="e-Commerce" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
+
+            <!-- Inicio del Servicio -->
+            <div class="col-md-3">
+                <div class="thumbnail">
+                    <a href="" data-toggle="modal" data-target="#servicio5">
+                        <div class="caption text-center">
+                            <h3>Pago de impuestos para Carros y Motos</h3>
+                            <br>
+                        </div>
+                        <img src="https://c1.staticflickr.com/3/2166/2247897607_3d1271e420_b.jpg" class="img-circle img-service" alt="Liquidación" width="200" height="200">
+                    </a>
+                    <div class="caption">
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- Final del Servicio -->
         </div>
     </div>
+</div>
+</div>
+
+<?php include 'modals/service.php' ?>
